68018b0ea91fcb4c3e36f82ce4522ab92547de15
jaromap4 <aporjarom@gmail.com>
2021-06-05 22:07:19 +0800

    second commit

--- controllers/survey.controller.js
+++ controllers/survey.controller.js
@@ -18,6 +18,7 @@ class SurveyController extends Controller {
     }
 
     async success(req, res) {
+        // success
         let response_data = await SurveyModel.getSurvey();
 		this.page_params.PAGE.title = "Success!";
         this.page_params.PAGE.view = "success";
