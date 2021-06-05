bca099e0141d191476921a2d0df3c72ec494d922
jaromap4 <aporjarom@gmail.com>
2021-06-05 22:21:26 +0800

    commit 5

--- controllers/survey.controller.js
+++ controllers/survey.controller.js
@@ -29,6 +29,7 @@ class SurveyController extends Controller {
         res.render("layouts/survey.layout.ejs", this.page_params);
     }
 
+    // commit 5
     async process(req, res) {
         await SurveyModel.createSurvey(req.body.name, req.body.dojo_location, req.body.fave_lang, req.body.comment);
         res.redirect("/success");
