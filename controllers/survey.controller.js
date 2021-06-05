25e190a218e0ae8563f078ba90d1dfac771a240d
jaromap4 <aporjarom@gmail.com>
2021-06-05 22:20:20 +0800

    fourth commit

--- controllers/survey.controller.js
+++ controllers/survey.controller.js
@@ -15,6 +15,7 @@ class SurveyController extends Controller {
         this.page_params.PAGE.assets.stylesheets.push({ file: `/public/css/custom/survey/${this.page_params.PAGE.view}.css` });
         
         res.render("layouts/survey.layout.ejs", this.page_params);
+        // fourth commit
     }
 
     async success(req, res) {
